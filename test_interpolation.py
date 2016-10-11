d6430ac8d4b1d5d0b0bea562082effc5129e93f5
Dominik Stańczak <stanczakdominik@gmail.com>
2016-10-11 22:10:21 +0200

    interpolation test

---
+++ test_interpolation.py
@@ -0,0 +1,27 @@
+from pic3 import *
+
+def test_poly():
+        NG = 128
+        L = 1
+
+        x, dx = np.linspace(0,L,NG, retstep=True,endpoint=False)
+        # charge_density = np.zeros_like(x)
+
+        x_particles = np.linspace(0, L, endpoint=False)
+        particle_charge = 1
+
+        for electric_field in np.ones_like(x), x, x**2:
+            interpolated = interpolateField(x_particles, electric_field, x)
+            def plot():
+                plt.plot(x, electric_field)
+                plt.plot(x_particles, interpolated, "go-")
+                plt.show()
+
+            grid_coeffs = np.polyfit(x, electric_field, 3)
+            part_coeffs = np.polyfit(x_particles, interpolated, 3)
+            # print(grid_coeffs, part_coeffs)
+            assert np.isclose(grid_coeffs, part_coeffs).all(), plot()
+        # charge_density = charge_density_deposition(x, dx, x_particles, particle_charge)
+
+if __name__=="__main__":
+    test_poly()
