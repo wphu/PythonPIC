3f9dd3a7290844345d6dab2015b83b33f67f6176
Dominik Stańczak <stanczakdominik@gmail.com>
2016-10-14 13:25:31 +0200

    remove halving of field energy start writing fourier solver docs

--- FourierSolver.py
+++ FourierSolver.py
@@ -3,8 +3,27 @@ import scipy.fftpack as fft
 import matplotlib.pyplot as plt
 
 def PoissonSolver(rho, x, epsilon_0 = 1):
-    #TODO: Aliasing: keep abs(delta_x k) < pi
-    #TODO: read arsenous blog
+    """solves the Poisson equation spectrally, via FFT
+
+    the Poisson equation can be written either as
+    (in position space)
+    $$\nabla \cdot E = \rho/\epsilon_0$$
+    $$\nabla^2 V = -\rho/\epsilon_0$$
+
+    Assuming that all functions in fourier space can be represented as
+    $$\exp{i(kx - \omega t)}$$   
+    It is easy to see that upon Fourier transformation $\nabla \to ik$, so
+
+    (in fourier space)
+    $$E = \rho /(ik \epsilon_0)$$
+    $$V = \rho / (-k^2 \epsilon_0)$$
+
+    Calculate that, fourier transform back to position space
+    and both the field and potential pop out easily
+
+    The conceptually problematic part is getting the $k$ wave vector right
+    #TODO: finish this description
+    """
     NG = len(x)
     dx = x[1]-x[0]
     rho_F = fft.fft(rho)
@@ -16,9 +35,6 @@ def PoissonSolver(rho, x, epsilon_0 = 1):
     field = fft.ifft(field_F).real
     potential = fft.ifft(potential_F).real
     energy = np.abs((0.5*np.sum(
-        (rho_F*potential_F.conjugate())[:NG//2]
+        (rho_F*potential_F.conjugate())
         )))
-    # import ipdb; ipdb.set_trace()
-    # field -= field.ptp()/2
-    # potential -= potential.ptp()/2
     return field, potential, energy
