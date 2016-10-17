023201ed4ec23f9a8b5671f06dbc8b3b493d2503
Dominik Stańczak <stanczakdominik@gmail.com>
2016-10-17 07:53:46 +0200

    autopep8 test_FourierSolver

--- test_FourierSolver.py
+++ test_FourierSolver.py
@@ -1,4 +1,5 @@
 from FourierSolver import *
+import matplotlib.pyplot as plt
 
 DEBUG = False
 
@@ -11,9 +12,9 @@ def test_PoissonSolver(debug=DEBUG):
     x, dx = np.linspace(0, L, NG, retstep=True, endpoint=False)
     charge_density = np.zeros_like(x)
 
-    charge_density = (2*np.pi)**2*np.sin(2*x*np.pi)
-    field = -2*np.pi*np.cos(2*np.pi*x)
-    potential = np.sin(2*np.pi*x)
+    charge_density = (2 * np.pi)**2 * np.sin(2 * x * np.pi)
+    field = -2 * np.pi * np.cos(2 * np.pi * x)
+    potential = np.sin(2 * np.pi * x)
 
     FSfield, FSpotential, FSenergy = PoissonSolver(charge_density, x)
 
@@ -23,10 +24,24 @@ def test_PoissonSolver(debug=DEBUG):
         ax0.plot(x, charge_density)
         ax0.set_title("Charge density")
         ax1.set_title("Field")
-        ax1.plot(x, FSfield, "r-", label="Fourier {:4.2f}".format(L2norm(field, FSfield)))
+        ax1.plot(
+            x,
+            FSfield,
+            "r-",
+            label="Fourier {:4.2f}".format(
+                L2norm(
+                    field,
+                    FSfield)))
         ax1.plot(x, field, "g-", label="Anal")
         ax2.set_title("Potential")
-        ax2.plot(x, FSpotential, "r-", label="Fourier {:4.2f}".format(L2norm(potential, FSpotential)))
+        ax2.plot(
+            x,
+            FSpotential,
+            "r-",
+            label="Fourier {:4.2f}".format(
+                L2norm(
+                    potential,
+                    FSpotential)))
         ax2.plot(x, potential, "g-", label="Anal")
         for ax in axes:
             ax.grid()
@@ -44,27 +59,33 @@ def test_PoissonSolver_complex(debug=DEBUG):
     N = 1000
     epsilon_0 = 1
     x, dx = np.linspace(0, L, N, retstep=True, endpoint=False)
-    anal_potential = np.sin(x*2*np.pi)+0.5*np.sin(x*6*np.pi)+0.1*np.sin(x*20*np.pi)
-    anal_field = -(2*np.pi*np.cos(x*2*np.pi)+3*np.pi*np.cos(x*6*np.pi)+20*np.pi*0.1*np.cos(x*20*np.pi))
-    charge_density = ((2*np.pi)**2*np.sin(x*2*np.pi)+18*np.pi**2*np.sin(x*6*np.pi)+(20*np.pi)**2*0.1*np.sin(x*20*np.pi))*epsilon_0
+    anal_potential = np.sin(x * 2 * np.pi) + 0.5 * \
+        np.sin(x * 6 * np.pi) + 0.1 * np.sin(x * 20 * np.pi)
+    anal_field = -(2 * np.pi * np.cos(x * 2 * np.pi) + 3 * np.pi *
+                   np.cos(x * 6 * np.pi) + 20 * np.pi * 0.1 * np.cos(x * 20 * np.pi))
+    charge_density = ((2 * np.pi)**2 * np.sin(x * 2 * np.pi) + 18 * np.pi**2 * np.sin(
+        x * 6 * np.pi) + (20 * np.pi)**2 * 0.1 * np.sin(x * 20 * np.pi)) * epsilon_0
 
     field, potential, energy = PoissonSolver(charge_density, x)
 
     def plots():
         fig, xspace = plt.subplots()
-        xspace.set_title(r"Solving the Poisson equation $\Delta \psi = \rho / \epsilon_0$ via Fourier transform")
+        xspace.set_title(
+            r"Solving the Poisson equation $\Delta \psi = \rho / \epsilon_0$ via Fourier transform")
         rhoplot, = xspace.plot(x, charge_density, "r-", label=r"\rho")
         Vplot, = xspace.plot(x, potential, "g-", label=r"$V$")
-        Eplot, = xspace.plot(x, field/np.max(np.abs(field)), "b-", label=r"$E$ (scaled)")
+        Eplot, = xspace.plot(x, field /
+                             np.max(np.abs(field)), "b-", label=r"$E$ (scaled)")
         EplotNoScale, = xspace.plot(x, field, "b--", label=r"$E$ (not scaled)")
         xspace.set_xlim(0, L)
         xspace.set_xlabel("$x$")
         xspace.grid()
-        xspace.legend((rhoplot, Vplot, Eplot, EplotNoScale), (r"$\rho$", r"$V$", r"$E$ (scaled)", "$E$ (not scaled)"))
+        xspace.legend((rhoplot, Vplot, Eplot, EplotNoScale),
+                      (r"$\rho$", r"$V$", r"$E$ (scaled)", "$E$ (not scaled)"))
         plt.show()
         return "test_PoissonSolver_complex failed!"
-    print(field-anal_field)
-    print(potential-anal_potential)
+    print(field - anal_field)
+    print(potential - anal_potential)
 
     field_correct = np.isclose(field, anal_field).all()
     potential_correct = np.isclose(potential, anal_potential).all()
@@ -78,8 +99,8 @@ def test_PoissonSolver_sheets(debug=DEBUG, test_charge_density=1):
 
     x, dx = np.linspace(0, L, NG, retstep=True, endpoint=False)
     charge_density = np.zeros_like(x)
-    region1 = (L*1/8 < x) * (x < L*2/8)
-    region2 = (L*5/8 < x) * (x < L*6/8)
+    region1 = (L * 1 / 8 < x) * (x < L * 2 / 8)
+    region2 = (L * 5 / 8 < x) * (x < L * 6 / 8)
     charge_density[region1] = test_charge_density
     charge_density[region2] = -test_charge_density
 
@@ -103,9 +124,13 @@ def test_PoissonSolver_sheets(debug=DEBUG, test_charge_density=1):
         return "test_PoissonSolver_sheets failed!"
 
     polynomial_coefficients = np.polyfit(x[region1], FSfield[region1], 1)
-    first_bump_right = np.isclose(polynomial_coefficients[0], test_charge_density, rtol=1e-2)
+    first_bump_right = np.isclose(
+        polynomial_coefficients[0],
+        test_charge_density,
+        rtol=1e-2)
     polynomial_coefficients = np.polyfit(x[region2], FSfield[region2], 1)
-    second_bump_right = np.isclose(polynomial_coefficients[0], -test_charge_density, rtol=1e-2)
+    second_bump_right = np.isclose(
+        polynomial_coefficients[0], -test_charge_density, rtol=1e-2)
     assert first_bump_right and second_bump_right, plots()
 
 
@@ -124,10 +149,10 @@ def test_PoissonSolver_ramp(debug=DEBUG):
     a = 1
 
     x, dx = np.linspace(0, L, NG, retstep=True, endpoint=False)
-    charge_density = a*x
+    charge_density = a * x
 
     FSfield, FSpotential, FSenergy = PoissonSolver(charge_density, x)
-    potential = -a*x**3/6
+    potential = -a * x**3 / 6
 
     def plots():
         fig, axes = plt.subplots(3)
@@ -146,7 +171,7 @@ def test_PoissonSolver_ramp(debug=DEBUG):
         return "test_PoissonSolver_ramp failed!"
 
     polynomial_coefficients = np.polyfit(x, FSpotential, 3)
-    assert np.isclose(polynomial_coefficients[0], -a/6,), plots()
+    assert np.isclose(polynomial_coefficients[0], -a / 6,), plots()
 
 if __name__ == "__main__":
-    test_PoissonSolver_sheets()
+    test_PoissonSolver_complex(True)
