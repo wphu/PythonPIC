bed9c83f462da9f56d235e05a8fd13e34e3c1b64
Dominik Stańczak <stanczakdominik@gmail.com>
2016-11-08 20:40:17 +0100

    cleaned up pic3

--- pic3.py
+++ pic3.py
@@ -34,17 +34,10 @@ def run(g, list_species, params, filename):
         total_energy = fourier_field_energy + total_kinetic_energy
         S.total_energy[i] = total_energy
         S.energy_per_mode[i] = g.energy_per_mode
-        # print("i{:4d} T{:12.3e} V{:12.3e} E{:12.3e}".format(i, total_kinetic_energy, fourier_field_energy, total_energy))
 
     runtime = time.time() - start_time
     print("Runtime: {}".format(runtime))
     S.save_data(filename=filename)
-
-    # return S.grid.k_plot, S.energy_per_mode.sum(axis=0)*S.dt/2
-
-    # ratio = S.kinetic_energy_history['electrons'].max()/S.field_energy.max()
-    # print("ratio ", ratio)
-    # return ratio
 
 def cold_plasma_oscillations(filename, plasma_frequency=1, qmratio=-1, dt=0.2, NT=150,
                              NG=32, N_electrons=128, L=2 * np.pi, epsilon_0=1,
@@ -53,6 +46,7 @@ def cold_plasma_oscillations(filename, plasma_frequency=1, qmratio=-1, dt=0.2, N
     """Implements cold plasma oscillations from Birdsall and Langdon
 
     (plasma excited by a single cosinusoidal mode via position displacements)"""
+    print("Running cold plasma oscillations")
     particle_charge = plasma_frequency**2 * L / float(N_electrons * epsilon_0 * qmratio)
     particle_mass = particle_charge / qmratio
 
@@ -67,19 +61,20 @@ def cold_plasma_oscillations(filename, plasma_frequency=1, qmratio=-1, dt=0.2, N
 
 def two_stream_instability(filename, plasma_frequency=1, qmratio=-1, dt=0.2, NT=300,
                              NG=32, N_electrons=128, L=2 * np.pi, epsilon_0=1,
-                             push_amplitude=0.001, push_mode=1, v1=1., v2=-1.):
+                             push_amplitude=0.001, push_mode=1, v0=1.0):
     """Implements two stream instability from Birdsall and Langdon"""
+    print("Running two stream instability")
     particle_charge = plasma_frequency**2 * L / float(2*N_electrons * epsilon_0 * qmratio)
     particle_mass = particle_charge / qmratio
 
     g = Grid(L=L, NG=NG)
     k0 = 2*np.pi/g.L
     w0 = plasma_frequency
-    print(k0*v1/w0)
+    print("k0*v0/w0 is", k0*v0/w0, "which means the regime is", "stable" if k0*v0/w0 < 2**0.5 else "unstable")
     electrons1 = Species(particle_charge, particle_mass, N_electrons, "beam1")
     electrons2 = Species(particle_charge, particle_mass, N_electrons, "beam2")
-    electrons1.v[:] = v1
-    electrons2.v[:] = v2
+    electrons1.v[:] = v0
+    electrons2.v[:] = -v0
     list_species = [electrons1, electrons2]
     for i, species in enumerate(list_species):
         species.distribute_uniformly(g.L, 0.5*g.dx*i)
@@ -94,25 +89,4 @@ if __name__ == "__main__":
     if args.filename[-5:] != ".hdf5":
         args.filename = args.filename + ".hdf5"
 
-    cold_plasma_oscillations(args.filename)
-    # cold_plasma_oscillations("1default.hdf5")
-    # cold_plasma_oscillations("2dense.hdf5", N_electrons=2**9+1)
-    # cold_plasma_oscillations("3leapfrog_instability_pre.hdf5", dt = 0.2, NT=150, N_electrons=128, NG=32)
-    # cold_plasma_oscillations("4leapfrog_instability_post.hdf5", dt = 3, NT=150, N_electrons=2**9+1, NG=32)
-    # cold_plasma_oscillations("5wavebreaking.hdf5", dt = 0.2, NT=150, N_electrons=2**11+1, NG=32, push_amplitude=2)
-    # cold_plasma_oscillations("6aliasing.hdf5", dt = 0.2, NT=150, N_electrons=2**9+1, NG=32, push_mode=18)
-    #
-    # two_stream_instability("ts1default.hdf5")
-    # two_stream_instability("ts2long.hdf5", NT=600)
-    # two_stream_instability("ts3growth.hdf5", N_electrons=4096, NT=600, v1=1, v2=-1, push_amplitude=0.01)
-    # two_stream_instability("ts4growth.hdf5", N_electrons=4096, NT=600, v1=2, v2=-2, push_amplitude=0.01)
-    # import matplotlib.pyplot as plt
-    # plt.figure()
-    # plt.plot(*res)
-    # plt.hlines(0.9952, 0, 16)
-    # plt.show()
-    # ratios = [cold_plasma_oscillations(args.filename, dt = 0.02, NT=1500, L=l, N_electrons=512) for l in L]
-    # print(ratios)
-    # plt.figure()
-    # plt.plot(L, ratios)
-    # plt.show()
+    two_stream_instability(args.filename)
