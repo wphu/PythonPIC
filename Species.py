4768116f9e77f062f4933b98cbc404fc82176d48
Dominik Stańczak <stanczakdominik@gmail.com>
2016-11-10 08:29:08 +0100

    Boris integrator for 1d3v simulation

--- Species.py
+++ Species.py
@@ -34,6 +34,28 @@ class Species(object):
         self.v = v_new
         return energy
 
+    def boris_push_particles(self, electric_field_function, magnetic_field_function,  dt, L):
+        """Boris pusher, unrelativistic"""
+        # add half electric impulse to v(t-dt/2)
+        vminus = self.v + self.q * electric_field_function(self.x) / self.m * dt * 0.5
+
+        # rotate to add magnetic field
+        t = -magnetic_field_function(self.x) * self.q / self.m * dt * 0.5
+        s = 2*t/(1+t*t)
+
+        vprime = vminus + np.cross(vminus, t) # TODO: axis?
+        vplus = vminus + np.cross(vprime, s)
+        v_new = vplus + self.q * electric_field_function(self.x) / self.m * dt * 0.5
+
+        self.x += v_new[:,0] * dt
+
+        self.x %= L
+        energy = self.v * v_new * (0.5 * self.m)
+        self.v = v_new
+        return energy
+        # add remaining half of electric impulse
+
+
     def distribute_uniformly(self, Lx, shift=False):
         self.x = (np.linspace(0, Lx, self.N, endpoint=False) + shift * self.N / Lx / 10) % Lx
 
@@ -50,3 +72,20 @@ class Species(object):
         result *= np.isclose(self.v, other.v).all()
         result *= self.name == other.name
         return result
+
+if __name__=="__main__":
+    import matplotlib.pyplot as plt
+    particles = Species(1, 1, 1, "particle")
+    particles.x = np.array([5], dtype=float)
+    particles.v = np.array([[0,1,0]], dtype=float)
+
+    x_history = []
+    v_history = []
+    T, dt = np.linspace(0, 10, 1000, retstep=True)
+    for t in T:
+        particles.boris_push_particles(lambda x: np.zeros_like(x), lambda x: np.array([[0,0,1]]), dt, np.inf)
+        x_history.append(particles.x[0])
+        v_history.append(particles.v[0,0])
+
+    plt.plot(x_history, v_history)
+    plt.show()
