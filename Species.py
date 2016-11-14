1d020a2c3d444e2f5cd3d31903830ff9f0597e9e
Dominik Stanczak <stanczakdominik@gmail.com>
2016-11-14 10:52:25 +0100

    Multiparticle Boris pusher

--- Species.py
+++ Species.py
@@ -34,10 +34,14 @@ class Species(object):
         self.v = v_new
         return energy
 
-    def boris_push_particles(self, electric_field_function, magnetic_field_function,  dt, L):
-        """Boris pusher, unrelativistic"""
+    def boris_init(self, electric_field_function, magnetic_field_function,  dt, L):
+        """Boris pusher initialization, unrelativistic"""
+
+        dt = -dt/2
         # add half electric impulse to v(t-dt/2)
-        vminus = self.v + self.q * electric_field_function(self.x) / self.m * dt * 0.5
+        efield = np.zeros((self.N, 3))
+        efield[:,0] = electric_field_function(self.x)
+        vminus = self.v + self.q * efield / self.m * dt * 0.5
 
         # rotate to add magnetic field
         t = -magnetic_field_function(self.x) * self.q / self.m * dt * 0.5
@@ -45,7 +49,26 @@ class Species(object):
 
         vprime = vminus + np.cross(vminus, t) # TODO: axis?
         vplus = vminus + np.cross(vprime, s)
-        v_new = vplus + self.q * electric_field_function(self.x) / self.m * dt * 0.5
+        v_new = vplus + self.q * efield / self.m * dt * 0.5
+
+        energy = self.v * v_new * (0.5 * self.m)
+        self.v = v_new
+        return energy
+
+    def boris_push_particles(self, electric_field_function, magnetic_field_function,  dt, L):
+        """Boris pusher, unrelativistic"""
+        # add half electric impulse to v(t-dt/2)
+        efield = np.zeros((self.N, 3))
+        efield[:,0] = electric_field_function(self.x)
+        vminus = self.v + self.q * efield / self.m * dt * 0.5
+
+        # rotate to add magnetic field
+        t = -magnetic_field_function(self.x) * self.q / self.m * dt * 0.5
+        s = 2*t/(1+t*t)
+
+        vprime = vminus + np.cross(vminus, t) # TODO: axis?
+        vplus = vminus + np.cross(vprime, s)
+        v_new = vplus + self.q * efield / self.m * dt * 0.5
 
         self.x += v_new[:,0] * dt
 
@@ -75,17 +98,18 @@ class Species(object):
 
 if __name__=="__main__":
     import matplotlib.pyplot as plt
-    particles = Species(1, 1, 1, "particle")
-    particles.x = np.array([5], dtype=float)
-    particles.v = np.array([[0,1,0]], dtype=float)
-
-    x_history = []
-    v_history = []
-    T, dt = np.linspace(0, 10, 1000, retstep=True)
-    for t in T:
-        particles.boris_push_particles(lambda x: np.zeros_like(x), lambda x: np.array([[0,0,1]]), dt, np.inf)
-        x_history.append(particles.x[0])
-        v_history.append(particles.v[0,0])
-
-    plt.plot(x_history, v_history)
+    particles = Species(1, 1, 4, "particle")
+    particles.x = np.array([5, 5, 5, 5], dtype=float)
+    # particles.v = np.array([[1,0,0],[0,2,0],[0,0,1],[1,1,1]], dtype=float)
+    particles.v = np.zeros((particles.N,3),dtype=float)
+    NT = 1000
+    x_history = np.empty((NT, particles.N))
+    v_history = np.empty((NT, particles.N))
+    T, dt = np.linspace(0, 2*np.pi*10, NT, retstep=True)
+    particles.boris_init(lambda x: np.arange(particles.N), lambda x: np.array(len(x)*[[0,0,1]]), dt, np.inf)
+    for i, t in enumerate(T):
+        x_history[i] = particles.x
+        v_history[i] = particles.v[:,0]
+        particles.boris_push_particles(lambda x: np.arange(particles.N), lambda x: np.array(len(x)*[[0,0,1]]), dt, np.inf)
+    plt.plot(x_history[:,:], v_history[:,:], ".-")
     plt.show()
