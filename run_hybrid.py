fc76e567d4d55b70e026f8a02dfec17c7da9754e
Dominik Stańczak <stanczakdominik@gmail.com>
2016-12-28 09:35:55 +0100

    BUGFIX: removed wildcard imports

--- run_hybrid.py
+++ run_hybrid.py
@@ -6,7 +6,8 @@ import plotting
 
 def hybrid_oscillations(filename, plasma_frequency=1, qmratio=-1, dt=0.2, NT=300,
                              NG=32, N_electrons=128, L=2 * np.pi, epsilon_0=1,
-                             push_amplitude=0.001, push_mode=1, v0=1.0):
+                             push_amplitude=0.001, push_mode=1, v0=1.0,
+                             cyclotron_frequency=1):
     """Implements hybrid oscillations from Birdsall and Langdon"""
     print("Running hybrid oscillations")
     particle_charge = plasma_frequency**2 * L / float(2*N_electrons * epsilon_0 * qmratio)
@@ -14,11 +15,15 @@ def hybrid_oscillations(filename, plasma_frequency=1, qmratio=-1, dt=0.2, NT=300
 
     g = Grid.Grid(L=L, NG=NG, NT=NT)
     electrons1 = Species(particle_charge, particle_mass, N_electrons, "electrons", NT=NT)
-    electrons1.v[:] = v0
+    electrons1.v[:,0] = v0
     list_species = [electrons1]
     for i, species in enumerate(list_species):
         species.distribute_uniformly(g.L, 0.5*g.dx*i)
         species.sinusoidal_position_perturbation(push_amplitude, push_mode, g.L)
+        species.sinusoidal_velocity_perturbation(1,
+            -cyclotron_frequency*push_amplitude,
+            push_mode,
+            g.L)
     params = NT, dt, epsilon_0
     return run(g, list_species, params, filename)
 
