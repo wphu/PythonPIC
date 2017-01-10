16bd5ee8ea851dbfe7ef25eec7856063d6c227c3
Dominik Stańczak <stanczakdominik@gmail.com>
2017-01-10 23:22:54 +0100

    Stopped on rotation matrix shape

--- rotation.py
+++ rotation.py
@@ -8,41 +8,45 @@ m = 1
 dt = 0.1
 c = 1
 def rotation_matrix(t, s):
-    result = np.eye(3)
-    sz = s[2]
-    sy = s[1]
-    tz = t[2]
-    ty = t[1]
+    result = np.zeros((N, 3, 3))
+    result[:] = np.eye(3)
+
+    sz = s[:,2]
+    sy = s[:,1]
+    tz = t[:,2]
+    ty = t[:,1]
     sztz = sz * tz
     syty = sy * ty
-    result[0,0] -= sztz
-    result[0,0] -= syty
-    result[0,1] = sz
-    result[1,0] = -sz
-    result[0,2] = -sy
-    result[2,0] = sy
-    result[1,1] -= sztz
-    result[2,2] -= syty
-    result[2,1] = sy*tz
-    result[1,2] = sz*ty
+    result[:,0,0] -= sztz
+    result[:,0,0] -= syty
+    result[:,0,1] = sz
+    result[:,1,0] = -sz
+    result[:,0,2] = -sy
+    result[:,2,0] = sy
+    result[:,1,1] -= sztz
+    result[:,2,2] -= syty
+    result[:,2,1] = sy*tz
+    result[:,1,2] = sz*ty
     return result
 
-x = np.array([1.])
-v = np.array([1., 0, 0])
+N = 10
+x = np.ones(N)
+v = np.array(N*[[1., 0, 0]])
 # E = np.array([1., 2., 3.])
-E = np.array([0., 0., 0.1])
+E = np.array(N*[[0., 0., 0.1]])
 # B = np.array([1., 2., 3.])
-B = np.array([0., 0., 1.])
+B = np.array(N*[[0., 0., 1.]])
+
 
 NT = 100
-x_history = np.zeros(NT)
-v_history = np.zeros((NT, 3))
+x_history = np.zeros((NT, N))
+v_history = np.zeros((NT, N, 3))
 time = np.arange(NT) * dt
 for i in range(NT):
     x_history[i] = x
     v_history[i] = v
     vminus = v + q * E / m * dt * 0.5
-    gamma_middle = np.sqrt(1+((vminus/c)**2).sum())
+    gamma_middle = np.sqrt(1+((vminus/c)**2).sum(axis=1, keepdims=True))
 
     # rotate to add magnetic field
     t = B * q * dt / (2 * m * gamma_middle)
@@ -50,9 +54,13 @@ for i in range(NT):
 
     rot = rotation_matrix(t, s)
 
-    vplus = rot @ vminus
+    # vplus = np.dot()
+    # for matrix, vector in zip(rot, vminus):
+    vplus = np.dot(rot, vminus)
+    import ipdb; ipdb.set_trace()
+
     v_new = vplus + q * E / m * dt * 0.5
-    gamma_new = np.sqrt(1+((vminus/c)**2).sum())
+    gamma_new = np.sqrt(1+((vminus/c)**2).sum(axis=1, keepdims=True))
     x_new = x + v_new[0] / gamma_new * dt
     x, v = x_new, v_new
 
