f05c47af518535956d833dfd7683a38e6c2bec9f
Dominik Stanczak <stanczakdominik@gmail.com>
2017-05-27 20:43:14 +0200

    Documentation in animation.py

--- pythonpic/visualization/animation.py
+++ pythonpic/visualization/animation.py
@@ -12,6 +12,17 @@ from ..algorithms.helper_functions import colors, directions
 
 # formatter = matplotlib.ticker.ScalarFormatter(useMathText=True, useOffset=False)
 class Plot:
+    """
+    A plot for visualization. Mainly an abstract class for overloading with interesting kinds of diagnostics.
+
+    Parameters
+    ----------
+    S : Simulation
+        A `Simulation` object to pull data from.
+    ax : matplotlib axis
+        An axis to draw on
+    """
+
     def __init__(self, S, ax):
         self.S = S
         self.ax = ax
@@ -28,17 +39,35 @@ class Plot:
         self.ax.yaxis.set_label_position("right")
 
     def animation_init(self):
+        """
+        Zeroes out all data in all lines of the plot. Useful for animation.
+        """
         for plot in self.plots:
             plot.set_data([], [])
 
     def update(self, i):
+        """
+        Updates the plot with information from a particular iteration of the simulation.
+
+        Parameters
+        ----------
+        i : int
+            Iteration of the simulation
+        """
         pass
 
     def return_animated(self):
+        """
+        Returns an iterable of all items that have changed. Useful for animation
+        """
         return self.plots
 
 
 class FrequencyPlot(Plot):
+    """
+    Plots the spatial Fourier transform of field energy versus wave number.
+    """  # TODO move the fourier analysis to PostProcessedGrid; describe the math here as well as there
+
     def __init__(self, S, ax):
         super().__init__(S, ax)
         self.plots.append(ax.plot([], [], "o-", label="energy per mode")[0])
@@ -53,7 +82,21 @@ class FrequencyPlot(Plot):
 
 
 def phaseplot_values(species):
-    return {"x": species.position_history,
+    """
+    A convenience function to get a dictionary of values, to allow generalization of the PhasePlot class.
+
+    The keys you can pull for phase plots are `x`, `v_x`, `v_y` and `v_z`.
+
+    Parameters
+    ----------
+    species : Species
+        A species to draw data from
+
+    Returns
+    -------
+    A dictionary of phase plot values.
+    """
+    return {"x":                           species.position_history,
             "v_x": species.velocity_history[:, :, 0],
             "v_y": species.velocity_history[:, :, 1],
             "v_z": species.velocity_history[:, :, 2],
@@ -61,6 +104,19 @@ def phaseplot_values(species):
 
 
 class PhasePlot(Plot):
+    """
+    Draws a phase plot.
+
+    The keys you can pull for phase plots are `x`, `v_x`, `v_y` and `v_z`.
+
+    Parameters
+    ----------
+    v1, v2 : str
+        keys for the phase plot.
+    alpha : float
+        An opacity value between 0 and 1. Useful for neat phase plots displaying density.
+    """
+
     def __init__(self, S, ax, v1, v2, alpha):
         super().__init__(S, ax)
         self.x = [phaseplot_values(species)[v1] for species in S.list_species]
@@ -84,6 +140,10 @@ class PhasePlot(Plot):
 
 
 class SpatialDistributionPlot(Plot):
+    """
+    Draws charge density from the grid.
+    """
+
     def __init__(self, S, ax):
         super().__init__(S, ax)
         self.plots.append(ax.plot([], [], "-", alpha=0.8)[0])
@@ -99,8 +159,21 @@ class SpatialDistributionPlot(Plot):
         self.plots[0].set_data(self.S.grid.x, self.S.grid.charge_density_history[i, :])
 
 
-class VelocityDistributionPlot(Plot):
-    def __init__(self, S, ax, v1="v_x"):
+class Histogram(Plot):
+    """
+    Draws a histogram of a given value from the phase plot dataset.
+
+    The keys you can pull for phase plots are `x`, `v_x`, `v_y` and `v_z`.
+
+    Parameters
+    ----------
+    v1 : str
+        A key to phase plot values.
+    n_bins: int
+        Number of bins to draw.
+    """
+
+    def __init__(self, S, ax, v1: str, n_bins: int = 50):
         super().__init__(S, ax)
         self.bin_arrays = []
         self.values = [phaseplot_values(species)[v1] for species in S.list_species]
@@ -108,10 +181,10 @@ class VelocityDistributionPlot(Plot):
             maxxs = max([np.max(np.abs(v)) for v in self.values])
             self.ax.set_xlim(-maxxs, maxxs)
         for i, s, v in zip(range(len(S.list_species)), S.list_species, self.values):
-            bin_array = np.linspace(v.min(), v.max())
+            bin_array = np.linspace(v.min(), v.max(), n_bins)
             self.bin_arrays.append(bin_array)
             self.plots.append(
-                ax.plot(*velocity_histogram_data(v[0], bin_array), colors[i])[0])
+                ax.plot(*calculate_histogram_data(v[0], bin_array), colors[i])[0])
         self.ax.set_xlabel(rf"${v1}$")
         self.ax.set_ylabel(r"Number of particles")
 
@@ -122,22 +195,36 @@ class VelocityDistributionPlot(Plot):
     def update(self, i):
         for species, histogram, bin_array, v in zip(self.S.list_species, self.plots, self.bin_arrays, self.values):
             index = helper_functions.convert_global_to_particle_iter(i, species.save_every_n_iterations)
-            histogram.set_data(*velocity_histogram_data(v[index], bin_array))
+            histogram.set_data(*calculate_histogram_data(v[index], bin_array))
 
 
-def velocity_histogram_data(arr, bins):
+def calculate_histogram_data(arr, bins):
     """
+    Calculates histogram values, normalized to the number of particles.
 
-    :param arr: particle velocity array
-    :param bins: number of bins or array of edges 
-    :return: x, y data on bins for linear plots
+    Parameters
+    ----------
+    arr : ndarray
+        Values of a particle property, for example, velocity
+    bins : ndarray
+        Bin edges for the histogram.
+    Returns
+    -------
+    bin_center : ndarray
+        Centers of histogram bars (the x array for plotting)
+    bin_height : ndarray
+        Heights of histogram bars (the y array for plotting)
     """
-    bin_height, bin_edge = np.histogram(arr, bins=bins)
+    bin_height, bin_edge = np.histogram(arr, bins=bins)  # TODO: this could be optimized
     bin_center = (bin_edge[:-1] + bin_edge[1:]) * 0.5
     return bin_center, bin_height
 
 
 class IterationCounter:
+    """
+    A little widget inserted on an axis, displaying the iteration number and current simulation time.
+    """
+
     def __init__(self, S, ax):
         self.S = S
         self.ax = ax
@@ -145,7 +232,7 @@ class IterationCounter:
                                verticalalignment='center', transform=ax.transAxes)
 
     def animation_init(self):
-        self.counter.set_text("Iteration: ")
+        self.update(0)
 
     def update(self, i):
         self.counter.set_text(f"Iteration: {i}/{self.S.NT}\nTime: {i*self.S.dt:.3g}/{self.S.NT*self.S.dt:.3g}")
@@ -155,6 +242,15 @@ class IterationCounter:
 
 
 class FieldPlot(Plot):
+    """
+    Draws electric and magnetic fields from the grid in a given direction
+
+    Parameters
+    ----------
+    j : int
+        Direction as Cartesian index number. 0: x, 1: y, 2: z
+    """
+
     def __init__(self, S, ax, j):
         super().__init__(S, ax)
         self.j = j
@@ -174,6 +270,15 @@ class FieldPlot(Plot):
 
 
 class CurrentPlot(Plot):
+    """
+    Draws currents from the grid in a given direction.
+
+    Parameters
+    ----------
+    j : int
+        Direction as Cartesian index number. 0: x, 1: y, 2: z
+    """
+
     def __init__(self, S, ax, j):
         super().__init__(S, ax)
         self.j = j
@@ -195,6 +300,18 @@ class CurrentPlot(Plot):
 
 
 class PlotSet:
+    """
+    A single object representing a few different plots on different axes.
+    Useful for plotting sets of directional values (fields, currents).
+
+    Parameters
+    ----------
+    axes : list
+        List of axes to use.
+    list_plots :
+        List of `Plot`s to update and return.
+    """
+
     def __init__(self, axes, list_plots):
         self.axes = axes
         self.list_plots = list_plots
@@ -212,18 +329,65 @@ class PlotSet:
 
 
 class TripleFieldPlot(PlotSet):
-    def __init__(self, S, axes):
+    """
+    Draws electric and magnetic field plots on the grid on a given list of axes.
+    Parameters
+    ----------
+    S : Simulation
+        Simulation to pull data from.
+    axes : list
+        List of matplotlib axes.
+    """
+
+    def __init__(self, S, axes: list):
+        assert len(axes) <= 3, "Too many axes, we ran out of directions!"
         plots = [FieldPlot(S, ax, j) for j, ax in enumerate(axes)]
         super().__init__(axes, plots)
 
 
 class TripleCurrentPlot(PlotSet):
-    def __init__(self, S, axes):
+    """
+    Draws currents on the grid on a given list of axes.
+    Parameters
+    ----------
+    S : Simulation
+        Simulation to pull data from.
+    axes : list
+        List of matplotlib axes.
+    """
+
+    def __init__(self, S, axes: list):
+        assert len(axes) <= 3, "Too many axes, we ran out of directions!"
         plots = [CurrentPlot(S, ax, j) for j, ax in enumerate(axes)]
         super().__init__(axes, plots)
 
 
 def animation(S, save: bool = False, alpha=1, frame_to_draw="animation"):
+    """
+
+    Creates an animation from `Plot`s.
+
+    Parameters
+    ----------
+    S : Simulation
+        Data source
+    save : bool
+        Whether to save the simulation
+    alpha : float
+        Opacity value from 0 to 1, used in the phase plot.
+    frame_to_draw : str, list or int
+        Default value is "animation" - this causes the full animation to be played.
+        A list such as [0, 10, 100, 500, 1000] causes the animation to plot iterations 0, 10, 100... etc
+        and save them as pictures. Does not display the frames.
+
+        An integer causes the animation to display a single iterations, optionally saving it as a picture.
+    Returns
+    -------
+    figure or matplotlib animation
+        Plot object, depending on `frame_to_draw`.
+    """
+    assert alpha <= 1, "alpha too large!"
+    assert alpha >= 0, "alpha must be between 0 and 1!"
     """ animates the simulation, showing:
     * grid charge vs grid position
     * grid electric field vs grid position
@@ -251,7 +415,7 @@ def animation(S, save: bool = False, alpha=1, frame_to_draw="animation"):
                         wspace=.25, hspace=0.6)  # TODO: remove particle windows if there are no particles
 
     phase_plot = PhasePlot(S, phase_axes, "x", "v_x", alpha)
-    velocity_histogram = VelocityDistributionPlot(S, distribution_axes)
+    velocity_histogram = Histogram(S, distribution_axes, "v_x")
     freq_plot = FrequencyPlot(S, freq_axes)
     charge_plot = SpatialDistributionPlot(S, charge_axis)
     iteration = IterationCounter(S, freq_axes)
@@ -280,11 +444,13 @@ def animation(S, save: bool = False, alpha=1, frame_to_draw="animation"):
                 plot.animation_init()
             return results
 
+        frames = np.arange(0, S.NT,
+                           helper_functions.calculate_particle_iter_step(S.NT),
+                           dtype=int),
 
         # noinspection PyTypeChecker
         animation_object = anim.FuncAnimation(fig, animate, interval=100,
-                                              frames=np.arange(0, S.NT, helper_functions.calculate_particle_iter_step(S.NT),
-                                                               dtype=int),
+                                              frames=frames,
                                               blit=True, init_func=init)
         if save:
             helper_functions.make_sure_path_exists(S.filename)
@@ -293,7 +459,14 @@ def animation(S, save: bool = False, alpha=1, frame_to_draw="animation"):
             animation_object.save(videofile_name, fps=15, writer='ffmpeg', extra_args=['-vcodec', 'libx264'])
             print(f"Saved animation to {videofile_name}")
         return animation_object
-    # elif isinstance(list) TODO: allow drawing snapshots
+    elif isinstance(frame_to_draw, list):
+        for i in frame_to_draw:
+            animate(i)
+            helper_functions.make_sure_path_exists(S.filename)
+            file_name = S.filename.replace(".hdf5", f"{i}.png")
+            print(f"Saving iteration {iteration} to {file_name}")
+            fig.savefig(file_name)
+        return fig
     elif isinstance(frame_to_draw, int):
         animate(iteration)
         if save:
