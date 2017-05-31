6f2ea9d849b4d15ab955180c79c50f35cee47f83
Dominik Stanczak <stanczakdominik@gmail.com>
2017-05-31 12:41:41 +0200

    Subclassify twostream

--- pythonpic/configs/run_twostream.py
+++ pythonpic/configs/run_twostream.py
@@ -15,7 +15,8 @@ def stability_condition(k0, v0, w0):
 
 
 category_name = "twostream"
-def two_stream_instability(filename,
+class two_stream_instablility(Simulation):
+    def __init__(self, filename,
                            plasma_frequency=1.,
                            qmratio=-1.,
                            T=300 * 0.2,
@@ -29,44 +30,46 @@ def two_stream_instability(filename,
                            vrandom=0.0,
                            save_data: bool = True,
                            species_2_sign=1):
-    """Implements two stream instability from Birdsall and Langdon"""
-    print("Running two stream instability")
-    grid = Grid(T=T, L=L, NG=NG, epsilon_0=epsilon_0)
-    print(f"plasma frequency: {plasma_frequency}")
-    print(f"timestep: {grid.dt}")
-    print(f"iloczyn: {plasma_frequency * grid.dt}")
+        """Implements two stream instability from Birdsall and Langdon"""
+        print("Running two stream instability")
+        grid = Grid(T=T, L=L, NG=NG, epsilon_0=epsilon_0)
+        print(f"plasma frequency: {plasma_frequency}")
+        print(f"timestep: {grid.dt}")
+        print(f"iloczyn: {plasma_frequency * grid.dt}")
 
-    physics.check_pusher_stability(plasma_frequency, grid.dt)
-    np.random.seed(0)
+        physics.check_pusher_stability(plasma_frequency, grid.dt)
+        np.random.seed(0)
 
-    particle_mass = 1
-    particle_charge = particle_mass * qmratio
-    scaling = abs(particle_mass * plasma_frequency ** 2 * L / float(
-        particle_charge * N_electrons * epsilon_0))
+        particle_mass = 1
+        particle_charge = particle_mass * qmratio
+        scaling = abs(particle_mass * plasma_frequency ** 2 * L / float(
+            particle_charge * N_electrons * epsilon_0))
 
-    physics.check_plasma_parameter(N_electrons * scaling, NG, grid.dx)
-    k0 = 2 * np.pi / L
+        physics.check_plasma_parameter(N_electrons * scaling, NG, grid.dx)
+        k0 = 2 * np.pi / L
 
-    expected_stability = stability_condition(k0, v0, plasma_frequency)
+        expected_stability = stability_condition(k0, v0, plasma_frequency)
 
-    electrons1 = Species(particle_charge, particle_mass, N_electrons, grid, "beam1", scaling=scaling)
-    electrons2 = Species(species_2_sign * particle_charge, particle_mass, N_electrons, grid, "beam2", scaling=scaling)
-    electrons1.v[:, 0] = v0
-    electrons2.v[:, 0] = -v0
-    list_species = [electrons1, electrons2]
-    for i, species in enumerate(list_species):
-        species.distribute_uniformly(L, 0.5 * grid.dx * i)
-        species.sinusoidal_position_perturbation(push_amplitude, push_mode, grid.L)
+        electrons1 = Species(particle_charge, particle_mass, N_electrons, grid, "beam1", scaling=scaling)
+        electrons2 = Species(species_2_sign * particle_charge, particle_mass, N_electrons, grid, "beam2", scaling=scaling)
+        electrons1.v[:, 0] = v0
+        electrons2.v[:, 0] = -v0
+        list_species = [electrons1, electrons2]
+        description = f"Two stream instability - two beams counterstreaming with $v_0$ {v0:.2f}"
         if vrandom:
-            species.random_velocity_perturbation(0, vrandom)
-    description = f"Two stream instability - two beams counterstreaming with $v_0$ {v0:.2f}"
-    if vrandom:
-        description += f" + thermal $v_1$ of standard dev. {vrandom:.2f}"
+            description += f" + thermal $v_1$ of standard dev. {vrandom:.2f}"
 
-    description += f" ({'stable' if expected_stability else 'unstable'}).\n"
-    run = Simulation(grid, list_species, filename=filename, category_type="twostream", title=description)
-    print(run)
-    # REFACTOR: add initial condition values to Simulation object
-    return run
+        description += f" ({'stable' if expected_stability else 'unstable'}).\n"
+        self.vrandom = vrandom
+        self.push_mode = push_mode
+        self.push_amplitude = push_amplitude
+        super().__init__(grid, list_species, filename=filename, category_type="twostream", title=description)
+
+    def grid_species_initialization(self):
+        for i, species in enumerate(self.list_species):
+            species.distribute_uniformly(self.grid.L, 0.5 * self.grid.dx * i)
+            species.sinusoidal_position_perturbation(self.push_amplitude, self.push_mode, self.grid.L)
+            if self.vrandom:
+                species.random_velocity_perturbation(0, self.vrandom)
 
 
