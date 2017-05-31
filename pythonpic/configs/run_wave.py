249d5465701f5aa37af70ec6f5d05f2d57416341
Dominik Stanczak <stanczakdominik@gmail.com>
2017-05-31 12:36:46 +0200

    Subclassify wave

--- pythonpic/configs/run_wave.py
+++ pythonpic/configs/run_wave.py
@@ -6,19 +6,19 @@ from ..algorithms import BoundaryCondition
 from ..classes import Grid, Simulation
 
 
-def wave_propagation(filename,
+class wave_propagation(Simulation):
+    def __init__(self, filename,
                      bc = BoundaryCondition.Laser(1, 1, 10, 3).laser_pulse,
                      ):
-    """Implements wave propagation"""
-    T = 50
-    NG = 60
-    L = 2 * np.pi
-    epsilon_0 = 1
-    c = 1
-    grid = Grid(T=T, L=L, NG=NG, epsilon_0=epsilon_0, c=c, bc=bc, periodic=False)
-    description = "Electrostatic wave driven by boundary condition"
+        """Implements wave propagation"""
+        T = 50
+        NG = 60
+        L = 2 * np.pi
+        epsilon_0 = 1
+        c = 1
+        grid = Grid(T=T, L=L, NG=NG, epsilon_0=epsilon_0, c=c, bc=bc, periodic=False)
+        description = "Electrostatic wave driven by boundary condition"
 
-    run = Simulation(grid, [], filename=filename, category_type="wave", title=description)
-    return run
+        super.__init__(grid, [], filename=filename, category_type="wave", title=description)
 
 
