8ff518e5133e5dc66324dd0cc60997ee29ff47cd
Dominik Stanczak <stanczakdominik@gmail.com>
2017-06-02 11:18:47 +0200

    Bring old tests back

--- pythonpic/configs/tests/test_beamplasma.py
+++ pythonpic/configs/tests/test_beamplasma.py
@@ -1,18 +1,18 @@
 # coding=utf-8
-# import pytest
-# from ..configs import weakbeam_instability
-# from ..algorithms.helper_functions import did_it_thermalize
-# from ..visualization.plotting import plots
-# from . import on_failure
+import pytest
+import numpy as np
+from ..run_beamplasma import weakbeam_instability, plots
+from pythonpic.helper_functions.physics import did_it_thermalize
+from . import on_failure
 #
-# @pytest.mark.parametrize(["N_beam", "should_it_thermalize"], [
-#     (128, False),
-#     (512, False),
-#     (2048, False),
-#     ])
-# def test_twostream_likeness(N_beam, should_it_thermalize):
-#     run_name = f"BP_TWOSTREAM_{N_beam}"
-#     S = weakbeam_instability(run_name, N_beam=N_beam,
-#                              save_data=False)
-#     assert (did_it_thermalize(S)[:2] == should_it_thermalize).all(), ("Incorrect thermalization",
-#                                                                       plots(S, *on_failure))
+@pytest.mark.parametrize(["L", "should_it_thermalize"], [
+    (2*np.pi, False),
+    (0.2*np.pi, False),
+    (100*np.pi, True),
+    ])
+def test_twostream_likeness(L, should_it_thermalize):
+    run_name = f"BP_TWOSTREAM_{N_beam}"
+    S = weakbeam_instability(run_name, L=L,
+                             save_data=False)
+    assert (did_it_thermalize(S)[:2] == should_it_thermalize).all(), ("Incorrect thermalization",
+                                                                      plots(S, *on_failure))
