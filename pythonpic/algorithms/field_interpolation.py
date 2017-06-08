5cdcac2cfe315b3eee689c415170c7a277ad7b65
Dominik Stanczak <stanczakdominik@gmail.com>
2017-06-08 10:45:37 +0200

    Numba compile on InterpolateField

--- pythonpic/algorithms/field_interpolation.py
+++ pythonpic/algorithms/field_interpolation.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numba import njit
 
+@njit()
 def PeriodicInterpolateField(x_particles, scalar_field, dx: float):
     """gathers field from grid to particles
 
@@ -8,13 +10,14 @@ def PeriodicInterpolateField(x_particles, scalar_field, dx: float):
     there is no need to use numpy.bincount as the map is
     not N (number of particles) to M (grid), but M to N, N >> M
     """
-    logical_coordinates = (x_particles / dx).astype(int)
+    logical_coordinates = (x_particles / dx).astype(np.int32)
     NG = scalar_field.shape[0]-2
-    right_fractions = (x_particles / dx - logical_coordinates)[:,np.newaxis]
+    right_fractions = (x_particles / dx - logical_coordinates).reshape((x_particles.size, 1))
     field = (1 - right_fractions) * scalar_field[logical_coordinates+1] + \
             right_fractions * scalar_field[(logical_coordinates +1) % NG  + 1]
     return field
 
+@njit()
 def AperiodicInterpolateField(x_particles, scalar_field, dx: float):
     """gathers field from grid to particles
 
@@ -23,9 +26,9 @@ def AperiodicInterpolateField(x_particles, scalar_field, dx: float):
     there is no need to use numpy.bincount as the map is
     not N (number of particles) to M (grid), but M to N, N >> M
     """
-    logical_coordinates = (x_particles / dx).astype(int)
+    logical_coordinates = (x_particles / dx).astype(np.int32)
     NG = scalar_field.shape[0]-2
-    right_fractions = (x_particles / dx - logical_coordinates)[:,np.newaxis]
+    right_fractions = (x_particles / dx - logical_coordinates).reshape((x_particles.size, 1))
     field = (1 - right_fractions) * scalar_field[logical_coordinates+1] + \
             right_fractions * scalar_field[logical_coordinates + 2]
     return field
