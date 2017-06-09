5ce4ee1fdf22cad4f7c6a087d18d69be796aa2a1
Dominik Stanczak <stanczakdominik@gmail.com>
2017-06-09 20:52:18 +0200

    Swap out Fourier for Buneman in old sims and destabilize twostream

--- twostream.py
+++ twostream.py
@@ -5,24 +5,26 @@ from pythonpic.configs.run_twostream import two_stream_instability, plots
 
 
 args = plotting_parser("Two stream instability")
-S = two_stream_instability("TS1",
-                       NG=512,
-                       N_electrons=4096,
-                       plasma_frequency=0.05 / 4,
-                       ).lazy_run()
-print(did_it_thermalize(S))
-plots(S, *args)
-S = two_stream_instability("TS2",
-                       NG=512,
-                       N_electrons=4096,
-                       plasma_frequency=0.05,
-                       ).lazy_run()
-print(did_it_thermalize(S))
-plots(S, *args)
+# S = two_stream_instability("TS1",
+#                        NG=512,
+#                        N_electrons=4096,
+#                        plasma_frequency=0.05 / 4,
+#                        ).lazy_run()
+# print(did_it_thermalize(S))
+# plots(S, *args)
+# S = two_stream_instability("TS2",
+#                        NG=512,
+#                        N_electrons=4096,
+#                        plasma_frequency=0.05 / 4,
+#                        v0 = 0.001,
+#                        ).lazy_run()
+# print(did_it_thermalize(S))
+# plots(S, *args)
 S = two_stream_instability("TS3",
                        NG=512,
                        N_electrons=4096,
-                       plasma_frequency=0.05 * 10,
+                       plasma_frequency=0.05 / 4,
+                       v0 = 0.0001,
                        ).lazy_run()
 print(did_it_thermalize(S))
 plots(S, *args)
